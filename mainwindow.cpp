f67a9f7347f5207c19d5904e805a56fa1d58f7dd
Hoàng Kiên <hoanglieuphuong@gmail.com>
2016-03-24 17:12:55 +0700

    24.03.2016
    Thêm , Xóa

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,12 +8,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     this->dbf = "D:/data.db";
     connectDB();
-    addMoneyDB("Thit",25000,QDate::currentDate());
-    addEventDB("Mua thi mat 15k");
-    closeDB();
+    initWidget();
+    loadMoneyTree();
 }
 
 MainWindow::~MainWindow(){
+    closeDB();
     delete ui;
 }
 
@@ -28,17 +28,6 @@ bool MainWindow::connectDB(){
         this->dbs = "CREATE TABLE IF NOT EXISTS user (username VARCHAR NOT NULL PRIMARY KEY, des VARCHAR);";
         if (!this->qry->exec(this->dbs)){
             qDebug() << "Error in create table User";
-            return false;
-        }
-        /*LOAD USER FROM TABLE*/
-        this->usrList.clear();
-        this->dbs = "SELECT * FROM User;";
-        if (this->qry->exec(this->dbs)){
-            while(this->qry->next()){
-                this->usrList.push_back(this->qry->value(0).toString());
-            }
-        }else{
-            qDebug() << "Error in reading User from database";
             return false;
         }
 
@@ -61,6 +50,7 @@ bool MainWindow::connectDB(){
                 return false;
             }
         }
+        loadUserList();
         if (this->usrList.count()){
             this->usr = * usrList.begin();
         }
@@ -81,13 +71,13 @@ QString MainWindow::generateID(){
 }
 
 /* Add money to Database*/
-bool MainWindow::addMoneyDB(const QString & item, int money, const QDate & date){
-    this->dbs = "INSERT INTO " + this->usr + "_MONEY values ('" + generateID() + "', '" + item + "', "
+bool MainWindow::addMoneyDB(const QString & item, int money, const QDate & date, const QString & id){
+    this->dbs = "INSERT INTO " + this->usr + "_MONEY values ('" + id + "', '" + item + "', "
             + QString::number(money) + ", '" + date.toString("yyyy-MM-dd") + "');";
     if (this->qry->exec(this->dbs)){
         return true;
     }
-    else{
+    else {
         qDebug() << "ERROR add item";
         return false;
     }
@@ -96,7 +86,7 @@ bool MainWindow::addMoneyDB(const QString & item, int money, const QDate & date)
 /* Delete a item by ID */
 bool MainWindow::delItemMoneyDB(const QString &id){
     this->dbs = "DELETE FROM " + this->usr + "_MONEY where id='" + id + "';";
-    if (!this->qry->exec(this->dbs)){
+    if (this->qry->exec(this->dbs)){
         return true;
     }
     else {
@@ -105,6 +95,7 @@ bool MainWindow::delItemMoneyDB(const QString &id){
     }
 }
 
+/* Add event to Database */
 bool MainWindow::addEventDB(const QString & event){
     this->dbs = "INSERT INTO " + this->usr + "_EVENT values ('" +
             QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss") +
@@ -118,6 +109,7 @@ bool MainWindow::addEventDB(const QString & event){
     }
 }
 
+/* Add user to Database */
 bool MainWindow::addUser(const QString & username, const QString & description){
     this->dbs = "INSERT INTO user values ('" + username + "', '" + description + "');";
     if (this->qry->exec(this->dbs)){
@@ -136,7 +128,7 @@ bool MainWindow::addUser(const QString & username, const QString & description){
         }
         return true;
     }
-    else{
+    else {
         qDebug() << "Error in add user";
         return false;
     }
@@ -156,7 +148,186 @@ bool MainWindow::removeUser(const QString &username){
         }
         return true;
     }
-    else{
+    else {
         return false;
+    }
+}
+
+/* Load User List */
+void MainWindow::loadUserList(){
+    /*LOAD USER FROM TABLE*/
+    this->usrList.clear();
+    this->dbs = "SELECT * FROM User;";
+    if (this->qry->exec(this->dbs)){
+        while(this->qry->next()){
+            this->usrList.push_back(this->qry->value(0).toString());
+        }
+    }
+    else  {
+        qDebug() << "Error in reading User from database";
+    }
+}
+
+/* Load user list to widget */
+void MainWindow::reloadUser(){
+    loadUserList();
+    ui->cmUser->addItems(this->usrList);
+}
+
+/* Init Widget, connect Widgets */
+void MainWindow::initWidget(){
+    reloadUser();
+    ui->date->setDate(QDate::currentDate());
+    ui->calendar->setSelectedDate(QDate::currentDate());
+    connect(ui->date,SIGNAL(dateChanged(QDate)),ui->calendar,SLOT(setSelectedDate(QDate)));
+    connect(ui->calendar,SIGNAL(selectionChanged()),this,SLOT(onCalendarChange()));
+    ui->moneytree->setColumnCount(3);
+    QTreeWidgetItem * header = ui->moneytree->headerItem();
+    header->setText(0,"Date");
+    header->setText(1,"Total Money");
+    header->setText(2,"ID");
+    loadMoneyTree();
+}
+
+/* Add Date - Toplevel Item into moneytree */
+QTreeWidgetItem *MainWindow::addDateMoneyTree(const QDate &date){
+    int count = ui->moneytree->topLevelItemCount();
+    for (int i = 0; i < count; i++){
+        if (ui->moneytree->topLevelItem(i)->text(0) == date.toString("dd/MM/yyyy")){
+            return ui->moneytree->topLevelItem(i);
+        }
+    }
+    QTreeWidgetItem * item = new QTreeWidgetItem;
+    item->setText(0,date.toString("dd/MM/yyyy"));
+    ui->moneytree->addTopLevelItem(item);
+    return item;
+}
+
+/* Add Money tree - Return this Item */
+QTreeWidgetItem * MainWindow::addItemMoneyTree(const QString &item, int money, const QDate & date, const QString & id){
+    QTreeWidgetItem * moneyitem = new QTreeWidgetItem;
+    moneyitem->setText(0,item);
+    moneyitem->setText(1,QString::number(money));
+    moneyitem->setText(2,id);
+    addDateMoneyTree(date)->addChild(moneyitem);
+    ui->moneytree->setCurrentItem(moneyitem);
+    updateMoneyTree(moneyitem->parent());
+    return moneyitem;
+}
+
+/* Load Money Tree */
+void MainWindow::loadMoneyTree(){
+    QString item;
+    int money;
+    QString id;
+    QDate date;
+    ui->moneytree->clear();
+    this->dbs = "SELECT * FROM " + this->usr + "_MONEY;";
+    if (this->qry->exec(this->dbs)){
+        while (qry->next()){
+            id = qry->value(0).toString();
+            item = qry->value(1).toString();
+            money = qry->value(2).toInt();
+            date = qry->value(3).toDate();
+            addItemMoneyTree(item,money,date,id);
+        }
+    }
+    updateMoneyTree();
+}
+
+/* Update Money for Money Tree */
+void MainWindow::updateMoneyTree(QTreeWidgetItem *item){
+    if (item){
+        if (item->parent()){
+            item = item->parent();
+        }
+        int count = item->childCount();
+        int total = 0;
+        for (int i = 0; i < count; i++){
+            total += item->child(i)->text(1).toInt();
+        }
+        item->setText(1,QString::number(total));
+    }
+    else {
+        int TopCount = ui->moneytree->topLevelItemCount();
+        QTreeWidgetItem * topItem ;
+        int childCount ;
+        int total;
+        for (int i = 0; i < TopCount; i++){
+            topItem = ui->moneytree->topLevelItem(i);
+            childCount = topItem->childCount();
+            total = 0;
+            for (int j = 0; j < childCount; j++){
+                total += topItem->child(j)->text(1).toInt();
+            }
+            topItem->setText(1,QString::number(total));
+        }
+    }
+}
+
+/* On calendar click change */
+void MainWindow::onCalendarChange(){
+    ui->date->setDate(ui->calendar->selectedDate());
+}
+
+void MainWindow::on_btnAdd_clicked(){
+    QString id ;
+    QString item;
+    QDate date;
+    int money;
+    if (ui->leItem->text() != ""){
+        item = ui->leItem->text();
+        money = ui->spMoney->value();
+        date = ui->date->date();
+        id = generateID();
+        if (addMoneyDB(item,money,date,id)){
+            addItemMoneyTree(item,money,date,id);
+            addEventDB("Added: " + item + " $ "+ QString::number(money) +" $ " + date.toString("dd/MM/yyyy"));
+            ui->leItem->setText("");
+            ui->spMoney->setValue(0);
+        }
+        else {
+            QMessageBox::critical(this,"ERROR","An Error occur, cannot add new item");
+            qDebug() << "ERROR: Cannot add new item";
+        }
+    }
+    else {
+        QMessageBox::critical(this,"ERROR","Item field cannot be empty");
+        qDebug() << "ERROR: Item field cannot be empty";
+    }
+}
+
+void MainWindow::on_btnDelete_clicked(){
+    QTreeWidgetItem * item = ui->moneytree->currentItem();
+    QTreeWidgetItem * parent = item->parent();
+    if (parent){
+        /* If it has parent Item */
+        if (QMessageBox::question(this,"Delete Item","Are You Sure Want To Delete This Item '" + item->text(0) +"' ?") == QMessageBox::Yes){
+            if (delItemMoneyDB(item->text(2))){
+                addEventDB("Deleted: " + item->text(0) + " $ " + item->text(1) + " $ " + item->parent()->text(0));
+                delete item;
+                if (parent->childCount() == 0){
+                    delete parent;
+                }
+                else {
+                    updateMoneyTree(parent);
+                }
+            }
+        }
+    }
+    else {
+        /* if item is TopLevelItem */
+        if (QMessageBox::question(this,"Delete Item","Are You Sure Want To Delete ALL Item in '"+ item->text(0) +"' ?") == QMessageBox::Yes){
+            int childCount = item->childCount();
+            for (int i = 0; i < childCount; i++){
+                if (delItemMoneyDB(item->child(i)->text(2))){
+                    addEventDB("Deleted: " + item->child(i)->text(0) + " $ " + item->child(i)->text(1) + " $ " + item->text(0));
+                }
+            }
+            while (item->childCount()){
+                delete item->child(0);
+            }
+            delete item;
+        }
     }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <QtSql>
 #include <QDateTime>
 #include <QMessageBox>
+#include <QTreeWidget>
 
 namespace Ui {
 class MainWindow;
@@ -18,22 +19,38 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-    /*For Database */
-    bool connectDB();
-    void closeDB();
-    QString generateID();
-    bool addMoneyDB(const QString & item, int money,const QDate & date);
-    bool delItemMoneyDB(const QString & id);
-    bool addEventDB(const QString & event);
-    bool addUser(const QString  & username, const QString & description);
-    bool removeUser(const QString & username);
+    /* For Database */
+    bool connectDB(); // Connect Database
+    void closeDB();   // Close conection database
+    QString generateID();   // Generate ID for insert money
+    bool addMoneyDB(const QString & item, int money,const QDate & date, const QString & id); // Add money to Database
+    bool delItemMoneyDB(const QString & id); // Delete a Item from Database
+    bool addEventDB(const QString & event);  // Add a event to Database
+    bool addUser(const QString  & username, const QString & description); // Add a user
+    bool removeUser(const QString & username);  // Remove a user
+    void loadUserList();
+
+    /* For Widgets */
+    void loadInfo();
+    void reloadUser();
+    void initWidget();
+    QTreeWidgetItem * addDateMoneyTree(const QDate & date);
+    QTreeWidgetItem * addItemMoneyTree(const QString &item, int money, const QDate & date, const QString & id);
+    void loadMoneyTree();
+    void updateMoneyTree(QTreeWidgetItem * item = 0);
+public slots:
+    void onCalendarChange();
+private slots:
+    void on_btnAdd_clicked();
+
+    void on_btnDelete_clicked();
 
 private:
     Ui::MainWindow *ui;
 
     /*User*/
-    QString usr;
-    QStringList usrList;
+    QString usr; // Current username
+    QStringList usrList; // List of Username: loaded by connectDB()
 
     /*For Database*/
     QSqlDatabase db; // Database
